0a008535fe3eb8f03000a10d362028968fb3271b
noah-behrendt407 <njbehrendt407@gmail.com>
2026-04-06 15:15:24 -0400

    redoing analysis stuff

--- analysis/merge_intersect_bulk_peaks.sh
+++ analysis/merge_intersect_bulk_peaks.sh
@@ -15,27 +15,26 @@ ml BEDTools/2.31.1-GCC-13.3.0
 # --- User Inputs ---
 PEAKS_DIR="/"
 OUTPUT_DIR=""
-OUTPUT_NAME="Consensus_species_Shared"
+OUTPUT_NAME="Consensus_replicates_Shared"
 
 mkdir -p "$OUTPUT_DIR"
 
-# Identify the two species files
-# Assumes files are named something like TIL11_collapsed.bed and TM_collapsed.bed
+# Identify the two replicates files
 FILES=($PEAKS_DIR/*.bed)
 FILE_A=${FILES[0]}
 FILE_B=${FILES[1]}
 
 echo "Processing Consensus for:"
-echo "Species A: $(basename $FILE_A)"
-echo "Species B: $(basename $FILE_B)"
+echo "Sample A: $(basename $FILE_A)"
+echo "Sample B: $(basename $FILE_B)"
 
 # --- Step 1: Create the Union (The "Footprint") ---
-# This merges both species together so we have the widest possible coverage
+# This merges both samples together so we have the widest possible coverage
 cat "$FILE_A" "$FILE_B" | sort -k1,1 -k2,2n | bedtools merge -i stdin > ${OUTPUT_DIR}/temp_union.bed
 
 # --- Step 2: Double Intersect (The "Filter") ---
-# Only keep peaks from the Union that overlap BOTH Species A and Species B
-# This ensures it isn't just a peak in one species
+# Only keep peaks from the Union that overlap BOTH Sample A and Sample B
+# This ensures it isn't just a peak in one sample
 bedtools intersect -a ${OUTPUT_DIR}/temp_union.bed -b "$FILE_A" -u | \
 bedtools intersect -a stdin -b "$FILE_B" -u > "${OUTPUT_DIR}/${OUTPUT_NAME}_peaks.bed"
 
@@ -48,6 +47,6 @@ echo "Final Conserved File: ${OUTPUT_DIR}/${OUTPUT_NAME}_peaks.bed"
 
 # --- Quality Check ---
 echo "Stats for your Lab Notebook:"
-echo "Species A Count: $(wc -l < $FILE_A)"
-echo "Species B Count: $(wc -l < $FILE_B)"
+echo "Sample A Count: $(wc -l < $FILE_A)"
+echo "Sample B Count: $(wc -l < $FILE_B)"
 echo "Consensus Shared Count: $(wc -l < ${OUTPUT_DIR}/${OUTPUT_NAME}_peaks.bed)"
